26b3671faed217743133fd48e9f4e8db0d9f2512
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-05-20 00:17:26 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { type NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const res = await fetch(`https://www.netlify.com/${req.nextUrl.pathname.replace('/-/media/', '')}`)
-  const headers = res.headers
+  const headers = new Headers(res.headers)
   headers.set('x-robbots-tag', 'follow, index')
   return new NextResponse(res.body, {
     headers,
